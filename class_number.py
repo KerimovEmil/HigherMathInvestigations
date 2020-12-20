6cfd5b2dc4b54c2c30c3c0db59a12a8e5a7baefc
KerimovEmil <emilkerimov93@gmail.com>
2020-12-19 21:44:35 -0500

    added some comments

--- class_number.py
+++ class_number.py
@@ -1,5 +1,10 @@
 import unittest
 
+# http://oeis.org/A000924
+# https://mathworld.wolfram.com/ClassNumber.html
+# https://www.springer.com/gp/book/9780387970370
+
+
 # reduced form |b| <= a <= c
 # discriminant = b^2 - 4ac
 # if discriminant = -D
@@ -21,10 +26,11 @@ def factors_of_n(n):
     return ls_out
 
 
-def get_class_number(D):
+def get_class_number(D, debug=False):
     """Given an <int> D, computes the class number of discriminant = -D of the binary quadratic form"""
     abs_b_max = int((D / 3) ** 0.5)
-
+    if debug:
+        print(f'max_abs_b: {abs_b_max}')
     count = 0
     for b in range(abs_b_max + 1):
         # temp = 4ac = b^2 + D
@@ -39,8 +45,10 @@ def get_class_number(D):
             ls_fac = factors_of_n(temp)
             for fac_tup in ls_fac:
                 if fac_tup[0] >= b and fac_tup[1] >= b:
-                    count += 1
+                    count += 1  # todo figure out why this is not +2, since b and -b both work
                     # print(b, b2, temp, fac_tup)
+                    if debug:
+                        print(f'b: {b}, a: {fac_tup[0]}, c: {fac_tup[1]}')
 
     return count
 
@@ -53,7 +61,18 @@ class TestPrimes(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    print('---CLASS NUMBER 1---')
     for i in range(1, 170):
         cn = get_class_number(i)
         if cn == 1:
             print(i)
+
+    print('---CLASS NUMBER 2---')  # todo this list is incorrect
+    for i in range(1, 170):
+        cn = get_class_number(i)
+        if cn == 2:
+            print(i)
+
+    print('---DEBUG 47---')
+    # this should equal to 5, not 3. Not sure if this is possible to compute.
+    print(get_class_number(47, debug=True))
