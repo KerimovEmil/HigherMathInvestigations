56668554475e836e56a29ce8c2d3931f68b77e9c
mchung-code <tkd.m.chung@gmail.com>
2021-01-08 17:20:55 -0700

    Add unittests for testing if matrix is vandermonde

--- matrix/basic_matrix.py
+++ matrix/basic_matrix.py
@@ -487,3 +487,25 @@ class TestMatrix(unittest.TestCase):
             [1 / 4, 1 / 5, 1 / 6, 1 / 7]])
 
         self.assertTrue(B.is_hankel())
+
+    def test_vandermonde(self):
+        B = Matrix(ls_entries=[[1, 1, 1],
+                               [4, 2, 1],
+                               [9, 3, 1],
+                               [25, 5, 1]])
+
+        self.assertTrue(B.is_vandermonde())
+
+        B = Matrix(ls_entries=[[1, 1, 1, 1],
+                               [8, 4, 2, 1],
+                               [27, 9, 3, 1],
+                               [125, 25, 5, 1]])
+
+        self.assertTrue(B.is_vandermonde())
+
+        B = Matrix(ls_entries=[[5, 1, 1, 1],
+                               [5, 4, 2, 1],
+                               [5, 9, 3, 1],
+                               [5, 25, 5, 1]])
+
+        self.assertFalse(B.is_vandermonde())
