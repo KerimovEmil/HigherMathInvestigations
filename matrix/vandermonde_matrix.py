6b42ddf9d5ed2801d3471efade7ccb735a1c9d57
mchung-code <tkd.m.chung@gmail.com>
2021-02-06 22:30:26 -0700

    Add comment for next idea to do

--- matrix/vandermonde_matrix.py
+++ matrix/vandermonde_matrix.py
@@ -1,6 +1,7 @@
 from matrix.basic_matrix import MatrixError, Matrix
 from matrix.square_matrix import SquareMatrix
 
+
 class VandermondeMatrix(Matrix):
     def __init__(self, ls_entries=None, matrix=None):
         if ls_entries is not None and isinstance(ls_entries, list):
@@ -12,6 +13,20 @@ class VandermondeMatrix(Matrix):
 
         if not self.is_vandermonde():
             raise MatrixError('Not a vandermonde matrix.')
+
+    def determinant(self):
+        # TODO
+        pass
+
+    def polynomial_fitting(self):
+        # TODO
+        pass
+
+    def lagrange_interpolation(self):
+        # TODO
+        # add plotting to show the differences between vandermonde fitting and lagrange
+        # https://mathrule.wordpress.com/2010/06/05/polynomial-interpolationvandermonde-matrix-lagrange-polynomial/
+        pass
 
 class SquareVandermondeMatrix(SquareMatrix, VandermondeMatrix):
     def __init__(self, ls_entries=None, matrix=None):
