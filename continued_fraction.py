82894b7af1aedec1826cde0885ba3f57aca15e38
KerimovEmil <emilkerimov93@gmail.com>
2021-08-13 01:15:06 -0400

    adding ability to specify function ratio directly

--- continued_fraction.py
+++ continued_fraction.py
@@ -1,7 +1,10 @@
 import decimal
 from fractions import Fraction
 from typing import Callable, Iterator, Union, Optional, List
-from math import sin, cos
+from math import sin, cos, tan
+# don't work with Fraction
+# from numpy import sin, cos
+# from mpmath import sin, cos, cot
 
 decimal.getcontext().prec = 100
 PI = decimal.Decimal('3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534'
@@ -51,9 +54,24 @@ class ContinuedFractionFunctionRoot:
     # https://www.ams.org/journals/mcom/1995-64-211/S0025-5718-1995-1297479-9/S0025-5718-1995-1297479-9.pdf
     # to get continued fraction without requiring a precise decimal expansion
     def __init__(self, f: Callable[[Union[float, Fraction]], float], f_prime: Callable[[Union[float, Fraction]], float],
+                 f_prime_ratio: Callable[[Union[float, Fraction]], float] = None,
                  min_n=None, ls_an=None, decimal_approx=None):
-        self.f = f
-        self.f_prime = f_prime
+        """
+
+        Args:
+            f: function such that simple root of f(x)=0 is number we want
+            f_prime: derivative of f
+            f_prime_ratio: f'/f
+            min_n:
+            ls_an:
+            decimal_approx:
+        """
+        if f_prime_ratio is None:
+            self.f = f
+            self.f_prime = f_prime
+            self.f_prime_ratio = None
+        else:
+            self.f_prime_ratio = f_prime_ratio
         self.b = 1/100  # can be determined from f'(x) and f''(x) somehow, but 1/100 works for most
 
         # get initial first few (only two needed) continued fraction values of the number
@@ -71,6 +89,12 @@ class ContinuedFractionFunctionRoot:
         # get the n-1 convergent
         c_nm1 = cf.static_fraction(self.ls_a_n)
 
+        # define the ratio of f'/f
+        if self.f_prime_ratio is None:
+            def f_prime_ratio(x): return self.f_prime(x) / self.f(x)
+        else:
+            f_prime_ratio = self.f_prime_ratio
+
         while n < max_n:  # get up to max_n terms in continued fraction expansion
 
             if c_nm2.numerator * c_nm1.denominator - c_nm1.numerator * c_nm2.denominator != (-1) ** n:
@@ -78,7 +102,8 @@ class ContinuedFractionFunctionRoot:
                 break
 
             # define the residual
-            func_ratio = (-1)**(n-1) * self.f_prime(c_nm1) / self.f(c_nm1)  # equivalent to abs(fund_ratio)
+            # func_ratio = (-1)**(n-1) * self.f_prime(c_nm1) / self.f(c_nm1)  # equivalent to abs(fund_ratio)
+            func_ratio = (-1)**(n-1) * f_prime_ratio(c_nm1)
             res = func_ratio / (c_nm1.denominator**2) - Fraction(c_nm2.denominator, c_nm1.denominator)
 
             # setting B >= y_n + 1, ensures that at least one B > y_n
@@ -126,6 +151,6 @@ if __name__ == '__main__':
     # # note that this does not work for transcendental numbers like pi, as an error rate needs to be introduced
     # # this is due to the fact that functions like sin(x) do not maintain the fraction class, hence lose precision
     # print('get continued fraction of pi by defining f(x) = sin(x)')
-    # cf_func_root = ContinuedFractionFunctionRoot(f=sin, f_prime=cos,
+    # cf_func_root = ContinuedFractionFunctionRoot(f=sin, f_prime=cos, f_prime_ratio=lambda x: 1/tan(x),
     #                                              decimal_approx=3.1415926535, min_n=4)
     # print(cf_func_root.get_ls_a_n(max_n=17))
