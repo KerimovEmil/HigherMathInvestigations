c7529f336c8911bd4149da8f2c1432f4dbaac081
KerimovEmil <emilkerimov93@gmail.com>
2023-02-25 12:37:05 -0500

    plotting robins_inequality plot

--- robins_inequality.py
+++ robins_inequality.py
@@ -4,6 +4,7 @@ https://en.wikipedia.org/wiki/Divisor_function
 from math import exp, log
 from divisor import divisors
 import matplotlib.pyplot as plt
+import matplotlib.style as style
 
 
 def harmonic(n: int) -> float:
@@ -21,12 +22,30 @@ def robin_ineq(n):
     return rhs - lhs
 
 
+def plotting(ls_x, ls_y):
+    style.use('ggplot')  # Set the plot style
+
+    fig, ax = plt.subplots(figsize=(8, 6))  # Set the plot size
+
+    ax.scatter(ls_x, ls_y, s=2, label='$h_n + e^{h_n}\ln(h_n) - \sigma(n)$')  # Use LaTeX to render label
+
+    ax.axvline(x=5040, ls='--', c='red', linewidth=1, label='starting value = 5040')  # Add vertical line at n=5040
+
+    ax.axhline(y=0, color='black', linestyle='--')  # Add horizontal line at y=0
+
+    ax.set_title("Robin's inequality", fontsize=20, fontweight='bold')  # Set the plot title
+    ax.set_xlabel("$n$", fontsize=16)  # Use LaTeX to render axis label
+    ax.set_ylabel("$h_n + e^{h_n}\ln(h_n) - \sigma(n)$", fontsize=16)  # Use LaTeX to render axis label
+    ax.tick_params(axis='both', which='major', labelsize=12)  # Set the font size of the tick labels
+
+    ax.legend(fontsize=12)  # Add the legend
+    plt.tight_layout()  # Make sure the plot is well-aligned
+
+    plt.show()
+
+
 if __name__ == '__main__':
-    n_range = range(5041, 10000)
+    n_range = range(5041, 10000)  # change upper bound to 20,000 to achieve the stored plot
     ls = [robin_ineq(n) for n in n_range]
 
-    plt.scatter(n_range, ls, s=5, label='h_n + e^{h_n}*ln(h_n) - sigma(n)')
-    plt.title("Robin's inequality")
-    plt.xlabel("n")
-    plt.ylabel("h_n + e^{h_n}*ln(h_n) - sigma(n)")
-    plt.show()
+    plotting(ls_x=n_range, ls_y=ls)
