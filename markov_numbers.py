33ada1b63a349522a2e6794861bbd866074570f5
KerimovEmil <emilkerimov93@gmail.com>
2023-12-29 13:12:15 -0500

    adding markov numbers

--- markov_numbers.py
+++ markov_numbers.py
@@ -15,8 +15,8 @@ x^2 + y^2 + (3xy-z)^2
 = 3xy(3xy - z)
 
 Markov Numbers: 1, 2, 5, 13, 29, 34, 89, 169, 194, 233, 433, 610, 985, 1325, 2897 ...
-LaGrange Numbers = sqrt(9 - 4/markov) =
-LaGrange Numbers^2 = 5, 7, 41/5, 113/13, 257/29, 151/17, 797/89, 1517/169
+LaGrange Numbers = sqrt(9 - 4/markov^2)
+LaGrange Numbers^2 = 5, 8, 221/25, 1517/169, 7565/841, 2600/289, ...
 
 Uniqueness Conjecture:
 if x<=y<=z, then given z in x^2 + y^2 + z^2 = 3xyz, (x,y) are uniquely determined.
@@ -30,6 +30,7 @@ class MarkovTuple:
         """(x,y,z) such that it satisfies Markov's equation."""
         self.ordered_tup = (min(x, y, z), (x + y + z) - min(x, y, z) - max(x, y, z), max(x, y, z))
 
+        # unproved theorem (uniqueness theorem)
         self.unique = max(x, y, z)
 
     def __str__(self):
@@ -51,17 +52,18 @@ class MarkovTuple:
         return self.unique == other.unique
 
     def __le__(self, other):
-        # unproved theorem (uniqueness theorem)
         return self.unique <= other.unique
 
     def __ge__(self, other):
-        # unproved theorem (uniqueness theorem)
         return self.unique >= other.unique
 
     def get_next_two_solutions(self):
         x, y, z = self.ordered_tup
         a = MarkovTuple(x, z, 3 * x * z - y)
         b = MarkovTuple(y, z, 3 * y * z - x)
+
+        # A is the smaller solution, I can't 100% confirm this, but it seems to be true
+        assert a.unique <= b.unique
 
         return [a, b]
 
@@ -70,8 +72,9 @@ def get_branching_sol(ls_sol):
     new_ls_sol = ls_sol.copy()
     for sol in ls_sol:
 
-        s1 = sol.get_next_small_solutions()
-        s2 = sol.get_next_large_solutions()
+        # s1 = sol.get_next_small_solutions()
+        # s2 = sol.get_next_large_solutions()
+        s1, s2 = sol.get_next_two_solutions()
 
         if s1 not in ls_sol:
             new_ls_sol.append(s1)
@@ -83,13 +86,13 @@ def get_branching_sol(ls_sol):
 
 
 def lagrange_sq_numbers(ls_markov):
-    """L_n^2 = 9 - 4/m"""
-    return [(9 - Fraction(4, m)) for m in ls_markov]
+    """L_n^2 = 9 - 4/m^2 = (9m^2 - 4)/m^2"""
+    return [(9 - Fraction(4, m**2)) for m in ls_markov]
 
 
 def lagrange_numbers(ls_markov):
-    """L_n = sqrt(9 - 4/m)"""
-    return [(9 - 4/m)**0.5 for m in ls_markov]
+    """L_n = sqrt(9 - 4/m^2) = sqrt(9m^2 - 4) / m"""
+    return [(9*m*m - 4)**0.5 / m for m in ls_markov]
 
 
 if __name__ == '__main__':
